3d9b96b65fdcd1c8a69a412465f5a0713677d511
Stefan Friedrich <friedrich.stef@icloud.com>
2026-07-30 12:05:11 +0200

    fix(logging): restrict file logging to DEBUG builds, reuse DateFormatter instance

--- Linio/PhoneDebugLogger.swift
+++ Linio/PhoneDebugLogger.swift
@@ -2,19 +2,26 @@ import Foundation
 import os.log
 
 private let _logger = Logger(subsystem: "com.stefanfriedrich.rnvapp", category: "PhoneDebug")
+
+#if DEBUG
 private let _logFile: URL? = {
     FileManager.default
         .containerURL(forSecurityApplicationGroupIdentifier: "group.com.stefanfriedrich.rnvapp")?
         .appendingPathComponent("phone_debug.log")
 }()
 private let _logQueue = DispatchQueue(label: "com.stefanfriedrich.rnvapp.debuglog")
+private let _logDateFormatter: DateFormatter = {
+    let f = DateFormatter()
+    f.dateFormat = "HH:mm:ss"
+    return f
+}()
+#endif
 
 func plog(_ msg: String) {
     _logger.debug("\(msg, privacy: .public)")
+#if DEBUG
     guard let url = _logFile else { return }
-    let f = DateFormatter()
-    f.dateFormat = "HH:mm:ss"
-    let line = "\(f.string(from: Date())) \(msg)\n"
+    let line = "\(_logDateFormatter.string(from: Date())) \(msg)\n"
     guard let data = line.data(using: .utf8) else { return }
     _logQueue.async {
         if FileManager.default.fileExists(atPath: url.path),
@@ -26,4 +33,5 @@ func plog(_ msg: String) {
             try? data.write(to: url)
         }
     }
+#endif
 }
